dc4a2612ccec6ae77e95cd673f977df8ce3dbd51
Madhusudhan <madhusudhanrk98@gmail.com>
2021-05-22 23:24:27 +0530

    python strings and it methods practice

---
+++ strings.py
@@ -0,0 +1,13 @@
+var = "Hi Madhusudhan Reddy"
+print(var[3:9])#prints 3 to 9 char in string arry
+print(var[:9])#prints 0 to 9 char in string arry
+print(var[:])#prints entire char in string arry
+print(var[6:])#prints 6 to end char in string arry
+print(var[-5:-1])#prints -1 to -5 last to first char in string arry
+print(var[-5:])#prints end to -5 last to first char in string arry 
+print(var.upper())#prints string in uppercase string method 
+print(var.lower())#prints string in lowercase string method 
+print(var.strip())#prints string without extra spaces(on space allowed) string method 
+print(var.replace("Hi","Hello"))#prints string with replaced word string method 
+print(var.split(" "))#prints string with spliting by value and convert to array.  
+print("hello"+"madhu"+" "+"MCA")#prints string with concatinate all values.  
