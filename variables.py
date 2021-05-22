8c06338b0f7853af879f2c2d6a4a095984e373cf
Madhusudhan <madhusudhanrk98@gmail.com>
2021-05-22 22:48:28 +0530

    python variables learned

---
+++ variables.py
@@ -0,0 +1,26 @@
+a=10
+b='madhu'
+c=2.0
+print(a)
+print(b)
+print(c)
+
+a= float(a)
+c=int(2.0)
+print("\n")
+print(a)
+print(b)
+print(c)
+
+a,b,c = 1,'mad',2.0
+print("\n")
+print(a)
+print(b)
+print(c)
+
+variable=_variable=VARIABLE=variable1 = 5
+print("\n")
+print(variable)
+print(_variable)
+print(VARIABLE)
+print(variable1)
