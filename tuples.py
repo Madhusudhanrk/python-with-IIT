cbfd1bbe8c878f9befd5d699723a6a067d3ae5b9
Madhusudhan <madhusudhanrk98@gmail.com>
2021-05-24 15:04:45 +0530

    tuples in python

---
+++ tuples.py
@@ -0,0 +1,11 @@
+#tuple is ORDERED,UNCHANGEABLE,ALLOW DUPLICATES
+cars = ("alto 800","fortuner","polo","hummer")
+print(cars)
+# cars.append("new car") values can't be added or updated
+
+#way to add value to tuples
+
+x = list(cars)
+x.append("new car")
+cars = tuple(x)
+print(cars)
