d21a66807085d4839fae28902a3d890b32cf7c91
Madhusudhan <madhusudhanrk98@gmail.com>
2021-06-05 23:14:31 +0530

    iterator using classes in python

--- iterators.py
+++ iterators.py
@@ -13,3 +13,27 @@ print(next(x))
 # x.__next__ also be used because it is a function and also a method
 print(next(x))
 # o/p 20 because next function will remember previous iterable position
+
+# WHAT IF NEED CUSTOMIZE ITERATOR
+class myNumber():
+    def __init__(self):
+        self.num = 100
+    def __iter__(self):
+        """This two functions will automatically initialize when object created and called"""
+        return self
+    def __next__(self):
+       
+       val = self.num
+       if val <= 100000:
+           self.num = self.num * 10
+           return val
+       else:
+           raise StopIteration
+
+x = myNumber()
+'''iterable function will return iter object and next function will return 1 by 1 values till infinite without any condition'''
+for y in x:#internally it uses __next__function
+    print(y)
+# print(x.__next__())
+# print(x.__next__())
+# print(x.__next__())
